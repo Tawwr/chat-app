93685fb7be90faed5c32c817f08c5b06db352fd3
moustafaElhadary <moustafaElhadary96@gmail.com>
2022-04-26 20:53:19 +0200

    Add redux

---
+++ client/src/redux/reducers/app.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { AppStateType, User } from '../../types'
+
+const initialState: AppStateType = {
+  user: null,
+  token: localStorage.getItem('chat-auth-token') || '',
+}
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    setToken(state, { payload }: { payload: string }) {
+      state.token = payload
+    },
+    setUser: (state, { payload }: { payload: User }) => {
+      state.user = payload
+    },
+    signOut: (state) => {
+      state.user = null
+      state.token = ''
+    },
+  },
+})
+
+export const { setUser, signOut, setToken } = appSlice.actions
+
+export default appSlice.reducer
