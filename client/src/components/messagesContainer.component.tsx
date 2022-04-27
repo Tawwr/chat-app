69cece0a8ed6fb8cad6f7205e583fdfaa180647a
Islam Sayed <islam.sayed8@gmail.com>
2022-04-27 23:14:27 +0200

    feat: add scroll to last msg when component mount, or update

--- client/src/components/messagesContainer.component.tsx
+++ client/src/components/messagesContainer.component.tsx
@@ -1,25 +1,42 @@
-import { Grid } from '@mui/material'
+import { Grid, Paper } from '@mui/material'
 import { DummyUser } from '../types'
 import Message from './message.component'
-
+import { useRef, useEffect } from 'react'
 type MessageContainerProps = {
   currentChat: DummyUser
 }
 const MessagesContainer = ({ currentChat }: MessageContainerProps) => {
+  const messagesWrapper = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (messagesWrapper.current) {
+      messagesWrapper.current.scrollTop = messagesWrapper.current.scrollHeight
+    }
+  }, [currentChat])
   return (
     <Grid
       container
       direction="column"
-      /* alignItems="flex-end" */ sx={{ padding: '0 30px', width: '100%' }}
+      wrap="nowrap"
+      justifyContent="flex-end"
+      sx={{
+        width: '100%',
+        height: '100%',
+      }}
     >
-      {currentChat.messages.map((message, idx) => (
-        <Message
-          key={idx}
-          message={message}
-          user={currentChat}
-          mobile={false}
-        />
-      ))}
+      <div
+        ref={messagesWrapper}
+        style={{ overflowY: 'auto', padding: '0 30px' }}
+      >
+        {currentChat.messages.map((message, idx) => (
+          <Message
+            key={idx}
+            message={message}
+            user={currentChat}
+            mobile={false}
+          />
+        ))}
+      </div>
     </Grid>
   )
 }
