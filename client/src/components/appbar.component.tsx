1b60ceb521fd059aee7c5730d0c26d80740a915b
Islam Sayed <islam.sayed8@gmail.com>
2022-04-28 13:23:46 +0200

    feat: add sign out btn and user avatar

--- client/src/components/appbar.component.tsx
+++ client/src/components/appbar.component.tsx
@@ -1,12 +1,17 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, Avatar, Button } from '@mui/material'
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signOut } from '../redux/reducers/app'
+import { RootState } from '../redux/store'
 
 type AppBarProps = {
   title: string | JSX.Element
 }
 const Appbar = ({ title }: AppBarProps) => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-
+  const dispatch = useDispatch()
+  //const user = useSelector((state: RootState) => state.app.user)
+  const user = true
   useEffect(() => {
     window.addEventListener('resize', () => {
       setScreenWidth(window.innerWidth)
@@ -18,6 +23,10 @@ const Appbar = ({ title }: AppBarProps) => {
       component="div"
       sx={{
         background: '#373759',
+        height: {
+          xs: '28px',
+          md: '32px',
+        },
         position: 'sticky',
         top: '0',
         left: '0',
@@ -26,19 +35,45 @@ const Appbar = ({ title }: AppBarProps) => {
         zIndex: '1',
       }}
     >
-      {screenWidth > 992 ? (
-        <Grid container>
+      {screenWidth > 768 ? (
+        <Grid container justifyContent="space-between">
           <Grid item>
             <img
               src={require('../assets/logo.svg').default}
               alt="logo"
               width="30"
+              style={{ marginRight: '8px' }}
             />
-          </Grid>
-          <Grid item>
-            <Typography variant="h6" color="#fff">
+            <Typography component="span" variant="h6" color="#fff">
               Connect
             </Typography>
+          </Grid>
+          <Grid item>
+            {user && (
+              <Grid
+                container
+                justifyContent="space-around"
+                sx={{ width: '200px' }}
+              >
+                <Grid item>
+                  <Avatar
+                    alt="Mahmoud"
+                    src="user-img.png"
+                    sx={{ width: 36, height: 36 }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="warning"
+                    size="small"
+                    onClick={() => dispatch(signOut())}
+                  >
+                    Sign out
+                  </Button>
+                </Grid>
+              </Grid>
+            )}
           </Grid>
         </Grid>
       ) : (
