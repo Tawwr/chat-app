ce34335260f35efa4d0d0c0591df652290df7047
moustafaElhadary <moustafaElhadary96@gmail.com>
2022-04-28 10:46:35 +0200

    [NOT COMPLETE]: Front End refactor

--- client/src/pages/auth.page.tsx
+++ client/src/pages/auth.page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { Grid, Paper, Typography } from '@mui/material'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 import Login from '../components/login.component'
@@ -6,17 +6,12 @@ import Signup from '../components/signup.component'
 import { RootState } from '../redux/store'
 
 type AuthProps = {
-  setTitle: (title: string | JSX.Element) => void
+  type: 'login' | 'signup'
 }
-const Auth = ({ setTitle }: AuthProps) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
+const Auth = ({ type }: AuthProps) => {
   const token = useSelector((state: RootState) => state.app.token)
 
-  const toggleAuth = () => setIsLoggedIn(!isLoggedIn)
 
-  useEffect(() => {
-    setTitle(isLoggedIn ? 'Login' : 'Signup')
-  }, [isLoggedIn])
 
   if (token) return <Navigate to="/" replace />
 
@@ -31,11 +26,66 @@ const Auth = ({ setTitle }: AuthProps) => {
         background: '#f4f5fb',
       }}
     >
-      {isLoggedIn ? (
-        <Login toggleAuth={toggleAuth} />
+       <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      sx={{ padding: '16px', height: '600px' }}
+    >
+      <Grid
+        item
+        md={5}
+        sx={{
+          height: '100%',
+          display: {
+            xs: 'none',
+            md: 'block',
+          },
+        }}
+      >
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            background: '#373759',
+            height: '100%',
+            borderRadius: '35px 0 0 35px',
+          }}
+        >
+          <Grid item>
+            <img src={require('../assets/logo.svg').default} alt="logo" />
+            <Typography align="center" variant="h4" color="#fff">
+              Welcome Back!
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item md={3} sx={{ height: '100%' }}>
+        <Paper
+          sx={{
+            height: '100%',
+            padding: '0 30px',
+            borderRadius: '0 35px  35px 0',
+          }}
+        >
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            sx={{ height: '100%' }}
+          >
+           {type === 'login' ? (
+        <Login />
       ) : (
-        <Signup toggleAuth={toggleAuth} />
+        <Signup />
       )}
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+      
     </div>
   )
 }
